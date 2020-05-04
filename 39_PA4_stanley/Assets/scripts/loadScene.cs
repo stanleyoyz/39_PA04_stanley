aa57e282fad241bed8401fb74faf685ac4868001
Tingwei-hub <64124325+Tingwei-hub@users.noreply.github.com>
2020-05-04 17:19:44 +0800

    Added new scene

---
+++ 39_PA4_stanley/Assets/scripts/loadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour
+{
+    public float delay = 1;
+    public string NewLevel = "CubeScene";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(NewLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
